3359b91c7af5578b6cbbbd990ce263bcbd771d30
msbae <tt1918@hanafos.com>
2025-05-12 18:20:33 +0900

    add) 미추가 함수 반영

---
+++ DefectDBManager/MarkrCompare/Helper/Functions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarkrCompare
+{
+    public static class Functions
+    {
+        public static void ChangeFont(Control parent, Font font)
+        {
+            parent.Font = font;
+            foreach (Control ctrl in parent.Controls)
+            {
+                ChangeFont(ctrl, font);
+            }
+        }
+
+        public static string GetCultureFontName(string curtule)
+        {
+            string name = "";
+            switch (curtule)
+            {
+                case "Ko-Kr":   name = "Segoe UI";              break;
+                case "en-US":   name = "Segoe UI";              break;
+                case "ja-JP":   name = "Yu Gothic UI";          break;
+                case "zh-CN":   name = "Microsoft YaHei UI";    break;
+                case "zh-TW":   name = "Microsoft JhengHei UI"; break;
+                default:        name = "Segoe UI";              break;
+            }
+
+            return name;
+        }
+    }
+}
