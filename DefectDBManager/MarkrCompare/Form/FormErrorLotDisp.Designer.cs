c736e5b0910ce9532f57b72cce3268c7715fa5f1
msbae <tt1918@hanafos.com>
2025-05-28 17:49:55 +0900

    add) 미첨부 파일 추가

---
+++ DefectDBManager/MarkrCompare/Form/FormErrorLotDisp.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace MarkCompare
+{
+    partial class FormErrorLotDisp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbLot = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnOK = new CustomControls.RoundButton();
+            this.lblTitle = new CustomControls.RoundLabel();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.btnOK, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 370);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(652, 49);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 85F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(658, 422);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel2.Controls.Add(this.lbLot, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(1, 94);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(656, 272);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // lbLot
+            // 
+            this.lbLot.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lbLot.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbLot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbLot.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLot.FormattingEnabled = true;
+            this.lbLot.ItemHeight = 21;
+            this.lbLot.Location = new System.Drawing.Point(13, 3);
+            this.lbLot.Name = "lbLot";
+            this.lbLot.Size = new System.Drawing.Size(630, 266);
+            this.lbLot.TabIndex = 0;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel4.Controls.Add(this.lblTitle, 1, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(1, 9);
+            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 83F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(656, 83);
+            this.tableLayoutPanel4.TabIndex = 5;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.DimGray;
+            this.btnOK.BackgroundColor = System.Drawing.Color.DimGray;
+            this.btnOK.BorderColor = System.Drawing.Color.Transparent;
+            this.btnOK.BorderRadius = 8;
+            this.btnOK.BorderSize = 0;
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnOK.FlatAppearance.BorderSize = 0;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Location = new System.Drawing.Point(554, 2);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(96, 45);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.TextColor = System.Drawing.Color.White;
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.BkColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.lblTitle.BorderColor = System.Drawing.Color.White;
+            this.lblTitle.CornerR = 15;
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTitle.IsFillLB = false;
+            this.lblTitle.IsFillLT = false;
+            this.lblTitle.IsFillRB = false;
+            this.lblTitle.IsFillRT = false;
+            this.lblTitle.Location = new System.Drawing.Point(11, 1);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(1);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.lblTitle.Size = new System.Drawing.Size(634, 81);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "오차 발생 LOT";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTitle.Thickness = 0;
+            // 
+            // FormErrorLotDisp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(658, 422);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormErrorLotDisp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FormErrorLotDisp";
+            this.TopMost = true;
+            this.VisibleChanged += new System.EventHandler(this.FormErrorLotDisp_VisibleChanged);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private CustomControls.RoundButton btnOK;
+        private CustomControls.RoundLabel lblTitle;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.ListBox lbLot;
+    }
+}
---
+++ DefectDBManager/MarkrCompare/Form/FormErrorLotDisp.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace MarkCompare
+{
+    public partial class FormErrorLotDisp : Form
+    {
+        #region Param
+        private BindingList<string> _errLots = new BindingList<string>();
+        DefectDBManager.Preproc.eProc _viewType;
+        #endregion
+
+        #region Form
+        public FormErrorLotDisp(DefectDBManager.Preproc.eProc type)
+        {
+            InitializeComponent();
+
+            lblTitle.MouseDown += lblTitle_MouseDown;
+            lblTitle.MouseMove += lblTitle_MouseMove;
+
+            // listBox에 바인딩
+            lbLot.DataSource = _errLots;
+
+            _viewType = type;
+
+            UpdateLanguage();
+        }
+
+        private void FormErrorLotDisp_VisibleChanged(object sender, EventArgs e)
+        {
+        }
+        #endregion
+
+        #region 마우스로 폼 드래그
+        private Point mouseDnLoc;
+        private void lblTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.mouseDnLoc = e.Location;
+            }
+        }
+        private void lblTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized) return;
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.Left = e.X + this.Left - this.mouseDnLoc.X;
+                this.Top = e.Y + this.Top - this.mouseDnLoc.Y;
+            }
+        }
+        #endregion
+
+        #region Control
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+           
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+        #endregion
+
+        #region Lot Update
+        public void OnUpdateErrorLots(string[] lots)
+        {
+            try
+            {
+                _errLots.RaiseListChangedEvents = false;// UI 갱신 중지
+                foreach (var lot in lots)
+                    _errLots.Add(lot);
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                _errLots.RaiseListChangedEvents = true;// UI 갱신 중지
+                _errLots.ResetBindings();
+            }
+        }
+        #endregion
+
+        #region 언어 변경
+        public void UpdateLanguage()
+        {
+            CultureInfo culture = CultureInfo.CurrentCulture;
+
+            string fontName = Functions.GetCultureFontName(culture.Name);
+            Font newFont = new Font(fontName, 10, FontStyle.Bold);
+
+            lblTitle.Font = newFont;
+            btnOK.Font = newFont;
+
+            if (_viewType == DefectDBManager.Preproc.eProc.Live)
+                lblTitle.Text = Lang.formErrorLotDispTitleLive;
+            else
+                lblTitle.Text = Lang.formErrorLotDispTitleSearch;
+
+            btnOK.Text = Lang.btnOK;
+        }
+        #endregion
+
+    }
+}
